88f271078581af966c6586eb8ed2e4e9b3d91ed2
TheNam74 <nguyenthenam1910@gmail.com>
2024-02-28 22:04:36 +0700

    cors fix

--- dist/main.js
+++ dist/main.js
@@ -14,20 +14,14 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: [
+      'http://main.d1rlk4399pr735.amplifyapp.com',
+      //   '*',
       //   'http://localhost:3000',
       //   'http://157.245.199.88:3000',
       //   'http://localhost:8000',
       //   'http://157.245.199.88:8000',
-      //   'http://student.dev.eworkbook.me',
-      //   'https://student.dev.eworkbook.me',
-      //   'http://teacher.dev.eworkbook.me',
-      //   'https://teacher.dev.eworkbook.me',
-      //   'https://student.eworkbook.me',
-      //   'https://teacher.eworkbook.me',
-      //   'https://main.d1rlk4399pr735.amplifyapp.com',
     ],
-    origin: '*',
-    // credentials: true,
+    credentials: true,
   });
   const customOptions = {
     swaggerOptions: {
